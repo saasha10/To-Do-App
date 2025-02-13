c6187f2e791c8be85840c1a4b5b381ed91ad9a45
saasha10 <50591864+saasha10@users.noreply.github.com>
2025-02-13 23:20:54 +0100

    Add input and checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css';
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
 import Header from './components/Header';
+import { Input, Flex } from "@chakra-ui/react"
+import { CheckboxCard } from './components/ui/checkbox-card';
+
 
 function App() {
   return (
     <ChakraProvider value={defaultSystem}>
       <Header title='To-Do List' />
+      <Input placeholder="Enter your task" />
+      <Flex>
+        <CheckboxCard label="Create app using Next.js" maxW="240px" size="sm" />
+      </Flex>
     </ChakraProvider>
   );
 }
