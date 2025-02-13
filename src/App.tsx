b505df22aeafa944be5de58ee01f4d181c94ae9a
saasha10 <50591864+saasha10@users.noreply.github.com>
2025-02-14 00:23:21 +0100

    Add input and list using state for add new tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,59 @@
 import './App.css';
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
+import { ChakraProvider, defaultSystem, Flex, Heading, Wrap, Button } from '@chakra-ui/react';
 import Header from './components/Header';
-import { Input, Flex } from "@chakra-ui/react"
+import { Input } from "@chakra-ui/react"
 import { CheckboxCard } from './components/ui/checkbox-card';
+import styled from 'styled-components';
+import { useState } from 'react';
 
+interface Item {
+  id: string,
+  title: string
+}
+
+const Wrapper = styled.div`
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`;
+
+const List = styled(Wrap)`
+  padding-top: 1rem;
+`;
 
 function App() {
+  const [toDoList, setToDoList] = useState<Item[]>([]);
+  const [newTaskTitle, setNewTaskTitle] = useState<string>("");
+
+  const cleanNewTaskTitle = () => setNewTaskTitle("");
+
+  const handleAddTask = () => {
+    const newTask: Item = {
+      id: Date.now().toString(),
+      title: newTaskTitle!
+    };
+
+    setToDoList(prevState => [...prevState, newTask]);
+    cleanNewTaskTitle();
+  };
+
   return (
     <ChakraProvider value={defaultSystem}>
       <Header title='To-Do List' />
-      <Input placeholder="Enter your task" />
       <Flex>
-        <CheckboxCard label="Create app using Next.js" maxW="240px" size="sm" />
+        <Wrapper>
+          <Heading>Add a new task</Heading>
+          <Input placeholder="Enter your task" maxW={"300px"} value={newTaskTitle} onChange={e => setNewTaskTitle(e.target.value)} />
+          <Button onClick={handleAddTask} disabled={!newTaskTitle}>Add task</Button>
+        </Wrapper>
+        <List>
+          {toDoList.length > 0 && (
+            toDoList.map(item => (
+              <CheckboxCard key={item.id} label={item.title} size='sm' />
+            ))
+          )}
+        </List>
       </Flex>
     </ChakraProvider>
   );
