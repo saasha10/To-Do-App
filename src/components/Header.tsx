de552dee4bb1ba99057c1a4d5f396640c3d8e9c7
saasha10 <50591864+saasha10@users.noreply.github.com>
2025-02-24 22:10:40 +0100

    Remove unneeded prop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,11 @@
 import { FC } from 'react';
 import { Box, Text } from '@chakra-ui/react';
 
-interface HeaderProps {
-  title: string;
-}
-
-const Header: FC<HeaderProps> = ({ title }) => {
+const Header: FC = () => {
   return (
     <Box as="header" p={4} bg="teal.500" color="white">
       <Text fontSize="2xl" fontWeight="bold">
-        {title}
+        To-Do List
       </Text>
     </Box>
   );
